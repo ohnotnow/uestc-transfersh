acb8fc30329d4e47ee9415c79be29c730a3fa422
Ohffs <ohffsnotnow@gmail.com>
2025-11-14 11:55:50 +0000

    WIP

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+BACKUP_FILES=(
+    "/path/to/stuff"
+    "/specific/file.txt"
+)
+
+TIMESTAMP=$(date +%Y%m%d)
+BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
+tar -czvf "$BACKUP_FILE" "${BACKUP_FILES[@]}"
+
+echo curl -u ${HTTP_AUTH_USER:-username}:${HTTP_AUTH_PASS:-secretpassword} --upload-file "$BACKUP_FILE" "https://hostname.example.ac.uk/$BACKUP_FILE"
+
+rm "$BACKUP_FILE"

7197ba6960eeeaf15389524e3afdd8831d051686
Ohffs <ohffsnotnow@gmail.com>
2025-11-14 14:54:51 +0000

    Faff

--- backup.sh
+++ backup.sh
@@ -11,6 +11,6 @@ TIMESTAMP=$(date +%Y%m%d)
 BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
 tar -czvf "$BACKUP_FILE" "${BACKUP_FILES[@]}"
 
-echo curl -u ${HTTP_AUTH_USER:-username}:${HTTP_AUTH_PASS:-secretpassword} --upload-file "$BACKUP_FILE" "https://hostname.example.ac.uk/$BACKUP_FILE"
+curl -u ${HTTP_AUTH_USER:-username}:${HTTP_AUTH_PASS:-secretpassword} --upload-file "$BACKUP_FILE" "https://hostname.example.ac.uk/$BACKUP_FILE"
 
 rm "$BACKUP_FILE"
